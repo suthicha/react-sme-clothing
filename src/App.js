44e1b89602ace3e1fe351f991e413ebd58dc7367
suthicha <suthicha@outlook.com>
2020-08-13 21:29:46 +0700

    added checkout page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
 
@@ -47,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path="/signin" render={ () => 
             this.props.currentUser ? 
             (<Redirect to='/' />) 
