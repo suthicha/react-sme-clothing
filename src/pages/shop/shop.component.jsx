20dfbbc1a0392a336ffa5d45aec700d2b0e8c44d
suthicha <suthicha@outlook.com>
2020-08-14 13:27:38 +0700

    update state normalization

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 
-import { createStructuredSelector } from 'reselect';
-import { selectShopItems } from '../../redux/shop/shop.selectors';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className="shop-page">
-        {
-            collections.map( ({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectShopItems
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
