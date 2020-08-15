e936d6ba1be7bb398c432519dc7062216541ee02
suthicha <suthicha@outlook.com>
2020-08-15 15:59:55 +0700

    added styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,30 +10,36 @@ import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
-import './header.styles.scss';
+import { 
+    HeaderContainer,
+    LogoContainer,
+    OptionContainer,
+    OptionLink,
+    OptionDiv
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className="logo" />
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">
+        </LogoContainer>
+        <OptionContainer>
+            <OptionLink to="/shop">
                 SHOP
-            </Link>
-            <Link className="option" to="/contact">
+            </OptionLink>
+            <OptionLink to="/contact">
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ? 
-                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
                 :
-                <Link className="option" to="/signin">SIGN IN</Link>
+                <OptionLink to="/signin">SIGN IN</OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionContainer>
         { hidden ? null : <CartDropDown /> }
-    </div>
+    </HeaderContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,8 @@
+import styles from 'styled-components';
+
+export const HomeContainer = styles.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 0px;
+`
