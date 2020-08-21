da9a4e6183e5586e9089aec7211cda2a02d4809c
suthicha <suthicha@outlook.com>
2020-08-21 16:33:57 +0700

    added redux-saga

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -15,16 +15,16 @@ export const fetchCollectionsFailure = errorMessage => ({
     payload: errorMessage,
 })
 
-export const fetchCollectionsStartAsync = () => {
-    return dispatch => {
-        const collectionRef = firestore.collection('collections');
-        dispatch(fetchCollectionsStart());
+// export const fetchCollectionsStartAsync = () => {
+//     return dispatch => {
+//         const collectionRef = firestore.collection('collections');
+//         dispatch(fetchCollectionsStart());
 
-        collectionRef.get().then(snapshot => {
-            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-            dispatch(fetchCollectionsSuccess(collectionMap));
-        }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
-    }
-}
+//         collectionRef.get().then(snapshot => {
+//             const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+//             dispatch(fetchCollectionsSuccess(collectionMap));
+//         }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
+//     }
+// }
 
 
